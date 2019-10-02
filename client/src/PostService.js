996dfe664f4d39c14ae827d84202033276586c97
joe <joe94008@gmail.com>
2019-10-02 22:51:35 +0800

    <feat>: 建構前端基本 vue cli 架構
    使用網路範本建立基本 vue cli 架構，之後會以此架構進行修改與擴增

---
+++ client/src/PostService.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+// const url = 'http://localhost:8080/api/posts/';
+const url = '/api/posts/';
+
+class PostService {
+    // Get Posts
+    static getPosts() {
+        return new Promise(async(resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(data.map(post => ({
+                    ...post,
+                    createdAt: new Date(post.createdAt)
+                })));
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    // Create Post
+    static insertPost(text) {
+        return axios.post(url, {
+            text: text
+        });
+    }
+
+    // Delete Post
+    static deletePost(id) {
+        return axios.delete(`${url}${id}`);
+    }
+}
+
+export default PostService;
